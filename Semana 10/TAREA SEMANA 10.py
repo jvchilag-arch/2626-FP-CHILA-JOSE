ead325ab9afeeef80f378a42ad40c8d6d35d328d
josevictorchilagonzalez <josevictorchilagonzalez@gmail.com>
2026-09-10 19:00:42 -0500

    Tarea semana 10

---
+++ Semana 10/TAREA SEMANA 10.py
@@ -0,0 +1,12 @@
+#matriz 3*3
+matriz = [
+    [2,4,6],
+    [1,3,5],
+    [7,8,9]
+]
+
+#recorrer la matriz
+for i in range(3):
+    for j in range(3):
+        print(matriz[i][j], end = " ")
+    print()
